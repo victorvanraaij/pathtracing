3e3d13778b43089160e10dedec8391bfa10c7444
Ralph Warrand <ralphwarrand@gmail.com>
2025-10-18 17:22:29 +0200

    Added diffuse reflections, environment mapping and some optimisations

--- shaders/pathtracing_SimplePS.glsl
+++ shaders/pathtracing_SimplePS.glsl
@@ -16,20 +16,24 @@ uniform vec2 vResolution;
 //camera position
 uniform vec4 vViewPosition;
 
+uniform sampler2D envMap; // Environment map texture
+
 // GUI elements   // TODO add more if needed
 uniform int averageCount;
 uniform int maxBounces;
+// How much a certain roughness scatters light
+uniform float scattering_amplitude;
+uniform bool spacial_jitter;
 uniform vec3 skyLight;
 uniform bool skyLightSwitch;
 uniform bool showGround;
 uniform bool showSphere;
-
+uniform bool showWalls;
+uniform bool showPillars;
+uniform float wallRoughness;
+uniform bool useEnvMap;
 uniform bool colorByNormal;
 uniform bool useGammaCorrection;
- 
-// How much a certain roughness scatters light
-uniform float scattering_amplitude;
- 
 uniform bool showLocalLight;
 uniform vec3 localLightColor;
 uniform float animationSpeed;
@@ -112,6 +116,7 @@ struct Object {
   bool isTransparent;
   bool isFloor;
   bool isLight;
+  bool isSky;
   
   mat4 m2w;    // model to world matrix
   mat4 w2m;    // world to model matrix
@@ -119,7 +124,7 @@ struct Object {
 };
 
 
-#define NO 10         // TODO: make sure to match the number of objects NO with the objects you add in animateObjects().
+#define NO 9         // TODO: make sure to match the number of objects NO with the objects you add in animateObjects().
 Object objects[NO];
 
 Object makeObject(bool enabled, int type, vec3 center, vec3 size, vec3 color, float roughness, mat4 T, mat4 R, mat4 S) {
@@ -133,10 +138,30 @@ Object makeObject(bool enabled, int type, vec3 center, vec3 size, vec3 color, fl
     obj.isTransparent = false;
     obj.isFloor = false;
     obj.isLight = false;
+    obj.isSky = false;
     obj.m2w = T * R * S;  // TODO
-    obj.w2m = inverse(T * R * S);  // TODO
-    // If TRS is orthonormal, taking the transpose and inverse is redundant
-    obj.nv  = transpose(inverse(T * R * S));  // TODO
+    //obj.w2m = inverse(T * R * S);  // TODO
+    
+    // --- OPTIMISATION ---
+    // Calculate inverses analytically
+    
+    // Inverse of Translate(v) is Translate(-v)
+    mat4 T_inv = Translate(-center);
+    
+    // Inverse of Rotate(theta) is Rotate(-theta), which is its transpose
+    mat4 R_inv = transpose(R); 
+    
+    // Inverse of Scale(v) is Scale(1.0 / v)
+    vec3 invSize = 1.0 / size;
+    mat4 S_inv = Scale(invSize);
+    
+    obj.w2m = S_inv * R_inv * T_inv;
+    
+    
+    // For T * R * S, the 3x3 normal matrix simplifies to R * S_inv
+    obj.nv = R * S_inv; 
+    obj.nv[3] = vec4(0,0,0,1);
+    
     return obj;
 }
 
@@ -154,6 +179,7 @@ void animateObjects(float t) {    // ToDo: add three walls ...
         mat4 S = Scale(size);
         objects[0]=makeObject(true, TYPE_SPHERE, center, size, color,1, T, R, S);
         objects[0].isLight=true;
+        objects[0].isSky = true;
     }
     
     {   // Shiny sphere
@@ -180,94 +206,98 @@ void animateObjects(float t) {    // ToDo: add three walls ...
     }
     
     {   // Local Light 1
-        // Animate in a circle
-        float radius = 2.f;
-        vec3 center = vec3(sin(animated_time) * radius, 5.0, cos(animated_time) * radius);
-        vec3 size = vec3(0.5); // Small sphere
+        vec3 center = vec3(0.0, 8, 0.0);
+        vec3 size = vec3(3.0,0.2,5.0);
         vec3 color = localLightColor;
-        float roughness = 1.0; // Doesn't matter for a light
+        float roughness = 1.0; //does not matter for lights
         mat4 T = Translate(center);
         mat4 R = mat4(1.0);
         mat4 S = Scale(size);
-        objects[3] = makeObject(showLocalLight, TYPE_SPHERE, center, size, color, roughness, T, R, S);
-        objects[3].isLight = true;
-    }
-    
-    {   // Local Light 2
-        // Animate in a circle
-        float radius = 2.f;
-        vec3 center = vec3(-sin(animated_time) * radius, 5.0, -cos(animated_time) * radius);
-        vec3 size = vec3(0.5); // Small sphere
-        vec3 color = localLightColor;
-        float roughness = 1.0; // Doesn't matter for a light
-        mat4 T = Translate(center);
-        mat4 R = mat4(1.0);
-        mat4 S = Scale(size);
-        objects[4] = makeObject(showLocalLight, TYPE_SPHERE, center, size, color, roughness, T, R, S);
-        objects[4].isLight = true;
+        objects[3]=makeObject(showLocalLight, TYPE_CUBE, center, size, color, roughness, T, R, S);
+        objects[3].isLight = true; 
     }
     
     {   // Back wall
-        vec3 center = vec3(0.0, 3.0, -13.0);
-        vec3 size = vec3(7.0, 6.0, 0.4);
+        vec3 center = vec3(0.0, 3.0, 13.0);
+        vec3 size = vec3(6, 6.0, 0.4);
         vec3 color = vec3(0.5,0.5,0.5);
-        float roughness = 1.0;
+        float roughness = wallRoughness;
         mat4 T = Translate(center);
         mat4 R = mat4(1.0);
         mat4 S = Scale(size);
-        objects[5]=makeObject(showGround, TYPE_CUBE, center, size, color, roughness, T, R, S);
-        objects[5].isFloor = false; 
+        objects[4]=makeObject(showWalls, TYPE_CUBE, center, size, color, roughness, T, R, S);
+        objects[4].isFloor = false; 
     }
     
     {   // Left wall
         vec3 center = vec3(-6.0, 3.0, 0.0);
         vec3 size = vec3(0.4, 6.0, 13.0);
         vec3 color = vec3(0,1,0);
-        float roughness = 1.0;
+        float roughness = wallRoughness;
         mat4 T = Translate(center);
         mat4 R = mat4(1.0);
         mat4 S = Scale(size);
-        objects[6]=makeObject(showGround, TYPE_CUBE, center, size, color, roughness, T, R, S);
-        objects[6].isFloor = false; 
+        objects[5]=makeObject(showWalls, TYPE_CUBE, center, size, color, roughness, T, R, S);
+        objects[5].isFloor = false; 
     }
     
     {   // Right wall
         vec3 center = vec3(6.0, 3.0, 0.0);
         vec3 size = vec3(0.4, 6.0, 13.0);
         vec3 color = vec3(1,0,0);
-        float roughness = 1.0;
+        float roughness = wallRoughness;
         mat4 T = Translate(center);
         mat4 R = mat4(1.0);
         mat4 S = Scale(size);
-        objects[7]=makeObject(showGround, TYPE_CUBE, center, size, color, roughness, T, R, S);
-        objects[7].isFloor = false; 
+        objects[6]=makeObject(showWalls, TYPE_CUBE, center, size, color, roughness, T, R, S);
+        objects[6].isFloor = false; 
     }
     
     {   // Pillar 1
-        vec3 center = vec3(-3.0, 1.0, -5.0);
+        vec3 center = vec3(-1.0, 1.0, 10.0);
         vec3 size = vec3(1.0, 3.0, 1.0);
         vec3 color = vec3(0.5,0.5,0.5);
         float roughness = 0.0f;
         mat4 T = Translate(center);
         mat4 R = RotateY(radians(45.f));
         mat4 S = Scale(size);
-        objects[8]=makeObject(showGround, TYPE_CUBE, center, size, color, roughness, T, R, S);
-        objects[8].isFloor = false; 
+        objects[7]=makeObject(showPillars, TYPE_CUBE, center, size, color, roughness, T, R, S);
+        objects[7].isFloor = false; 
     }
     
     {   // Pillar 2
-        vec3 center = vec3(3.0, 1.0, -5.0);
+        vec3 center = vec3(3.0, 1.0, 10.0);
         vec3 size = vec3(1.0, 3.0, 1.0);
         vec3 color = vec3(0.5,0.5,0.5);
         float roughness = 0.0f;
         mat4 T = Translate(center);
-        mat4 R = RotateY(-animated_time);
+        mat4 R = RotateY(animated_time);
         mat4 S = Scale(size);
-        objects[9]=makeObject(showGround, TYPE_CUBE, center, size, color, roughness, T, R, S);
-        objects[9].isFloor = false; 
+        objects[8]=makeObject(showPillars, TYPE_CUBE, center, size, color, roughness, T, R, S);
+        objects[8].isFloor = false; 
     }
     
     // TODO: add objects here when needed
+}
+
+// Creates a local coordinate system (U, V) around the normal (W)
+void createOrthoNormalBasis(in vec3 W, out vec3 U, out vec3 V) {
+    // Find a vector that's not parallel to W
+    vec3 T = (abs(W.x) > 0.9) ? vec3(0, 1, 0) : vec3(1, 0, 0);
+    
+    // U is the "right" vector
+    U = normalize(cross(T, W));
+    
+    // V is the "forward" vector
+    V = cross(W, U);
+}
+
+// Converts a 3D direction vector to 2D UV coordinate for the sky map using "Equirectangular" mapping
+vec2 getEnvMapUV(vec3 dir) {
+    vec3 normDir = normalize(dir);
+    float u = 0.5 + atan(normDir.z, normDir.x) / (2.0 * radians(180.f));
+    float v = 0.5 + asin(normDir.y) / radians(180.f);
+    return vec2(u, v);
 }
 
 /* returns signed distances (t0,t1) to the intersections of ray and sphere, t0 <= t1; 
@@ -298,10 +328,13 @@ vec2 rayUnitSphereIntersect(vec3 r0, vec3 rd) { // TODO
 /* returns signed distances (t0,t1) to the intersections of ray and cube, t0 <= t1; 
    if there are no intersections (-1,-1) is returned.
 */
-vec2 rayUnitBoxIntersect(vec3 r0, vec3 rd) {   // TODO
+vec2 rayUnitBoxIntersect(vec3 r0, vec3 rd) {
+	// Precalc inverse direction
+	vec3 invDir = 1.0 / rd;
+
     // Compute intersection distances with the cube face planes
-    vec3 tp = (vec3(-1) - r0) / rd; // Postive faces
-    vec3 tn = (vec3(1) - r0) / rd; // Negative faces
+    vec3 tp = (vec3(-1) - r0) * invDir; // Postive faces
+    vec3 tn = (vec3(1) - r0) * invDir; // Negative faces
 
     // Ensure tmin < tmax per axis
     // Unfortunately I don't think this works if multiple t-values are negative, i.e.
@@ -349,7 +382,10 @@ HitData AllObjectsRayTest(vec3 rayPos, vec3 rayDir)
     HitData hitData;
     hitData.closest = 999999.0; //keeps track of distance to closest intersection
 
-	animateObjects(fTime);
+	// Originally animate objects was called here which means it gets called every pixelm sample and bounce
+	// We moved it to begining of main. 
+	// does not cause a noticeable improvement in performance, but for large scenes it might
+	//animateObjects(fTime);
 	
     //all objects
     for(int i = 0; i < NO; i++)
@@ -416,6 +452,24 @@ HitData AllObjectsRayTest(vec3 rayPos, vec3 rayDir)
 float rand01(float seed) { return fract(sin(seed)*43758.5453123); }
 
 
+// Generates a random direction in the hemisphere around the normal W, with a cosine weight
+vec3 cosineSampleHemisphere(in float r1, in float r2, in vec3 U, in vec3 V, in vec3 W) {
+    float pi = 3.14159265;
+    
+    // Create a random point on a 2D disk
+    float r = sqrt(r1);
+    float theta = 2.0 * pi * r2;
+    float x = r * cos(theta);
+    float y = r * sin(theta);
+    
+    // Project that 2D point up onto the unit hemisphere
+    // We implement Malley's method
+    float z = sqrt(1.0 - r1); // r1 is r*r
+    
+    // Go from local space (x,y,z) back to world space using our basis
+    return normalize(x * U + y * V + z * W);
+}
+
 //---------------------------------------------------------------------
 vec3 calculateFinalColor(vec3 cameraPos, vec3 cameraRayDir, float AAIndex)
 {
@@ -438,7 +492,16 @@ vec3 calculateFinalColor(vec3 cameraPos, vec3 cameraRayDir, float AAIndex)
         HitData h = AllObjectsRayTest(rayOrigin + rayDir * 0.0001, rayDir);
         
        // rays end at light source
-	   if (h.obj.isLight) {	finalColor = h.obj.color * absorbMul;	break; }
+	   if (h.obj.isLight) {	
+	   		if (h.obj.isSky && useEnvMap) {
+                // It's the sky sphere AND we want to use the map
+                vec2 uv = getEnvMapUV(rayDir);
+                finalColor = texture(envMap, uv).rgb * absorbMul;
+            } else {
+                finalColor = h.obj.color * absorbMul;
+            }
+            break;
+	   }
                    
         // The new ray origin is the point of intersection in world space
         rayOrigin = (h.obj.m2w * h.intersection).xyz;
@@ -463,23 +526,61 @@ vec3 calculateFinalColor(vec3 cameraPos, vec3 cameraRayDir, float AAIndex)
         float seed_x = float(i) * 123.456 + AAIndex * 434.578 + ray_hash;
         float seed_y = float(i) * 789.123 + AAIndex * 734.978 + ray_hash;
         float seed_z = float(i) * 456.789 + AAIndex * 897.709 + ray_hash;
+               
+        // Here we will decide whether to do a specular or diffuse bounce
+        float bounce_type_rand = rand01(seed_x);
         
-       	float random_angle_x = scattering_amplitude * h.obj.roughness * (rand01(seed_x) - 0.5);
-        float random_angle_y = scattering_amplitude * h.obj.roughness * (rand01(seed_y) - 0.5);
-        float random_angle_z = scattering_amplitude * h.obj.roughness * (rand01(seed_z) - 0.5);
-        
-        // Rotate the normal by the random angles. This makes physical sense, as we're
-        // directly simulating the non-uniformity of rough suface normals.
-        // For an object with zero roughness, the normal should stay the same
-        normal = mat3(RotateX(random_angle_x)) * mat3(RotateY(random_angle_y)) * mat3(RotateZ(random_angle_z)) * normal;
-        
-        // We obtain the new direction by reflecting the ray w.r.t surface normal
-        rayDir = reflect(rayDir, normal);
+        if (bounce_type_rand < h.obj.roughness)  // Diffuse bounce
+        {
+           // Create the local coordinate system
+            vec3 U, V;
+            createOrthoNormalBasis(normal, U, V);
+            
+            // Get two new random numbers for our hemisphere direction
+            float r1 = rand01(seed_y);
+            float r2 = rand01(seed_z);
+            
+            // The new ray direction is a random cosine-weighted vector
+            rayDir = cosineSampleHemisphere(r1, r2, U, V, normal);
+        }
+        else	// Specular bounce
+        {
+       		float random_angle_x = scattering_amplitude * h.obj.roughness * (rand01(seed_x) - 0.5);
+        	float random_angle_y = scattering_amplitude * h.obj.roughness * (rand01(seed_y) - 0.5);
+        	float random_angle_z = scattering_amplitude * h.obj.roughness * (rand01(seed_z) - 0.5);
+            
+            // Rotate the normal by the random angles. This makes physical sense, as we're
+        	// directly simulating the non-uniformity of rough suface normals.
+        	// For an object with zero roughness, the normal should stay the same
+            vec3 perturbed_normal = mat3(RotateX(random_angle_x)) * mat3(RotateY(random_angle_y)) * mat3(RotateZ(random_angle_z)) * normal;
+            
+            // We obtain the new direction by reflecting the ray w.r.t the (possibly perturbed) normal
+            rayDir = reflect(rayDir, perturbed_normal);
+        }
+       
       
         // every bounce absorbs some light (more bounces = darker scene)
         //     - contains a hack for the checkerboard pattern of the floor
         float intensityFactor = (!h.obj.isFloor) ? 1.0: mod(int(200+rayOrigin.x) + int(200+rayOrigin.z),2);
         absorbMul *= (intensityFactor * h.obj.color);
+        
+        // Performance optimisation strategy: Russian Roullete
+		// Start terminating rays after 3 bounces
+		if (i > 3) {
+    		// Get the max component of the color
+    		float p = max(absorbMul.r, max(absorbMul.g, absorbMul.b));
+    
+    		// Set a minimum survival chance to avoid 0/0
+    		float survival_prob = max(0.1, p); 
+
+    		float seed_rr = float(i) * 123.456 + AAIndex * 789.123 + ray_hash;
+    		if (rand01(seed_rr) > survival_prob) {
+       			break; // Terminate this path
+    		}
+    
+    		// Boost the energy of paths that survive
+    		absorbMul /= survival_prob;
+		}
         	   
         if (colorByNormal)  {   // debugging option to see your normals
        		finalColor = 0.5 * (normal + vec3(1.0)); // maps [-1,1] to [0,1]
@@ -506,10 +607,30 @@ vec4 computeColor( vec2 fragCoord )
     
     //camera-to-fragment ray direction
     vec3 rayDir = normalize(3*cameraDir + vec3(uv, 0));
+    
+    float ray_hash = dot(rayDir, vec3(12.9898, 78.233, 151.719));
 
     vec3 finalColor = vec3(0);
     for(int i = 1; i <= averageCount; i++)
     {
+		if(spacial_jitter) {
+			float seed1 = float(i) * 123.456 + texCoord.x * 434.578 + ray_hash;
+        	float seed2 = float(i) * 789.123 + texCoord.y * 734.978 + ray_hash;
+        
+        	// Random offset from -0.5 to +0.5 pixels
+        	float offsetX = (rand01(seed1) - 0.5) / vResolution.x;
+        	float offsetY = (rand01(seed2) - 0.5) / vResolution.y;
+        	
+        	// Apply jitter to the normalized fragment coordinate
+        	vec2 jitteredFragCoord = texCoord + vec2(offsetX, offsetY);
+        	
+        	// Recalculate UV and ray direction FOR EACH sample
+        	vec2 uv = jitteredFragCoord * 2.0 - 1.0;
+        	uv.x *= vResolution.x / vResolution.y;
+        	rayDir = normalize(3*cameraDir + vec3(uv, 0));
+		}
+    
+    
         finalColor += calculateFinalColor(cameraPos, rayDir, float(i));
     }
     finalColor = finalColor/float(averageCount);//brute force AA & denoise
@@ -524,6 +645,8 @@ vec4 computeColor( vec2 fragCoord )
 
 void main(void)
 {
+	animateObjects(fTime);
+
     gl_FragColor = computeColor(texCoord);
 }
 
